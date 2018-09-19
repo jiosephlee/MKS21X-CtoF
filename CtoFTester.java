c108d5f0272144268439cf1993d1dd89f2fce8a1
Joseph Lee <jlee01@stuy.edu>
2018-09-19 18:04:56 -0400

    Created java file

---
+++ CtoFTester.java
@@ -0,0 +1,10 @@
+public class CtoFTester{
+    public static int celciusToFahrenheit(int temp){
+      return temp × 1.8 + 32;
+    }
+}
+public class CtoFTester{
+    public static int fahrenheitToCelsius(int temp){
+        return (temp - 32) / 1.8;
+    }
+}
