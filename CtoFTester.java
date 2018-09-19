d570b9e2cee73302dd6709347c97a1a04715d688
Joseph Lee <jlee01@stuy.edu>
2018-09-19 18:44:56 -0400

    fixed some errors

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,15 +1,14 @@
 public class CtoFTester{
 
-
+    public static void main(Strings[] arg){
+      System.out.println(celciusToFahrenheit(5));
+      System.out.println(fahrenheitToCelsius(5));
+    }
     public static double celciusToFahrenheit(int temp){
       return temp * 1.8 + 32;
     }
 
     public static double fahrenheitToCelsius(int temp){
         return (temp - 32) / 1.8;
-    }
-    public static void main(){
-      System.out.println(celciusToFahrenheit(5));
-      System.out.println(fahrenheitToCelsius(5));
     }
 }
