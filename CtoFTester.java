6f5aba92595d5b6be9c89ad397e5e9a785480e4d
Joseph Lee <jlee01@stuy.edu>
2018-09-19 18:32:31 -0400

    Fixed some of the code, ran into an error

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,17 +1,15 @@
 public class CtoFTester{
 
 
-  public static void main(double arg){
-    System.out.println(celciusToFahrenheit(arg));
-    System.out.println(fahrenheitToCelsius(arg));
-  }
-
-    public static int celciusToFahrenheit(int temp){
-      return temp × 1.8 + 32;
+    public static double celciusToFahrenheit(int temp){
+      return temp * 1.8 + 32;
     }
 
-    public static int fahrenheitToCelsius(int temp){
+    public static double fahrenheitToCelsius(int temp){
         return (temp - 32) / 1.8;
     }
-    
+    public static void main(){
+      System.out.println(celciusToFahrenheit(5));
+      System.out.println(fahrenheitToCelsius(5));
+    }
 }
