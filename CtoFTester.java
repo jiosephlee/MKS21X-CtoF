1baeb06fb9862a8719854385b7322e0740f13499
Joseph Lee <jlee01@stuy.edu>
2018-09-19 18:15:27 -0400

    Wrote code

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,10 +1,17 @@
 public class CtoFTester{
+
+
+  public static void main(double arg){
+    System.out.println(celciusToFahrenheit(arg));
+    System.out.println(fahrenheitToCelsius(arg));
+  }
+
     public static int celciusToFahrenheit(int temp){
       return temp × 1.8 + 32;
     }
-}
-public class CtoFTester{
+
     public static int fahrenheitToCelsius(int temp){
         return (temp - 32) / 1.8;
     }
+    
 }
